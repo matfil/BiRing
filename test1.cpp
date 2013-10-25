15742cfb696eab9ebf06401c31a5bc4ad72ea25a
Mateusz <matfil@linux-matfil.site>
2013-10-25 15:43:32 +0200

    tests

---
+++ test1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "BIRING.cpp"
+
+const int STOP = 1<<31;
+
+int main()
+{
+BiRing<int> test;
+BiRing<int>::iterator it;
+int data[] = { 1, 2, -3, 10, 10, 10, 8, 7, 14, 5, 0, STOP};
+for(int i=0; data[i]!=STOP; ++i)
+	{
+	test.push_back(data[i]);
+	}
+for(it = test.begin(); it != test.end(); ++it)
+	{
+	cout << *it << " ";
+	}
+cout << endl;
+return 0;
+}
---
+++ test3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "BIRING.cpp"
+
+const int SIZE = 500, MAXS = 10000;
+
+int main(int argc, char ** argv)
+{
+BiRing<int> test;
+BiRing<int>::iterator it;
+if (argc >= 2)
+	{
+	srand(atoi(argv[1]));
+	}
+else
+	srand( time(NULL) );
+for(int i=0; i< SIZE; ++i)
+	{
+	test.push_back(rand() % (2*MAXS) - MAXS);
+	}
+int q;
+q=0;
+for(it = test.begin(); it != test.end(); ++it)
+	{
+	++q;
+	cout.width(5);
+	cout << *it;
+	if(!(q%5)) cout << endl;
+	else cout << " ";
+	}
+cout << "size: " << test.size() << endl;
+for(int i=0; i < SIZE/5; ++i)
+	{
+	int to_del = rand() % test.size();
+	it = test.begin();
+	for(int j=0; j<to_del; ++j)
+		++it;
+	test.remove(it);
+	}
+q=0;
+for(it = test.begin(); it != test.end(); ++it)
+	{
+	++q;
+	cout.width(5);
+	cout << *it;
+	if(!(q%5)) cout << endl;
+	else cout << " ";
+	}
+cout << "size: " << test.size() << endl;
+return 0;
+}
